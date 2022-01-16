76032dc3cdaf733f7a20965cbb63af260138a37e
Ben Kelly <Benjamin.J.Kelly@live.com>
2022-01-16 17:57:57 -0500

    Give Heroku the ability to pull in data from DVC upon app start up

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
 import joblib
 import pandas as pd
+import os
 from starter.starter.ml.model import inference
 from starter.starter.ml.data import process_data, get_cat_features
 
 from pydantic import BaseModel
+
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system("dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
 
 class CensusEntry(BaseModel):
     age: int
