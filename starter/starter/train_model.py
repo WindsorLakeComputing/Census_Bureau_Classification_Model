50ca50a7bedc2b7a690a12102cb73a815537e02a
Ben Kelly <Benjamin.J.Kelly@live.com>
2022-01-18 18:01:55 -0500

    Minor update

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -3,8 +3,8 @@
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import joblib
-from ml.data import process_data, get_cat_features
-from ml.model import train_model, compute_model_metrics, inference
+from .ml.data import process_data, get_cat_features
+from .ml.model import train_model, compute_model_metrics, inference
 
 def train_lgbm_model(train_data, label):
     X_train, y_train, encoder, lb = process_data(
@@ -35,6 +35,16 @@ def make_predictions(model, label, encoder, lb, test_data):
 
     return preds, y_test
 
+def test_preditions(preds, y_test):
+    precision, recall, beta = compute_model_metrics(y_test, preds)
+
+    print("precision is")
+    print(precision)
+    print("recall is")
+    print(recall)
+    print("beta")
+    print(beta)
+
 def print_model_metrics(file_name, test_data, label, encoder, lb, model, categorical_col):
     f = open(file_name, "w")
     for unc_e in test_data[categorical_col].unique():
@@ -49,4 +59,5 @@ if __name__ == "__main__":
     train_data, test_data = get_train_test_data("../data/clean_census.csv", .2)
     lgbm_class, encoder, lb = train_lgbm_model(train_data, "salary")
     preds, y_test = make_predictions(lgbm_class, "salary", encoder, lb, test_data)
+    test_preditions(preds, y_test)
     print_model_metrics("slice_output.txt", test_data, "salary", encoder, lb, lgbm_class, "education")
