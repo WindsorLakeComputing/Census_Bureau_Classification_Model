f80cdda973b28a224fe3e112c59e5b0a7dad0bc4
Ben Kelly <Benjamin.J.Kelly@live.com>
2022-01-18 20:33:10 -0500

    Revert tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,8 @@
 import pytest
 import pandas as pd
 import joblib
-from ..starter.starter.train_model import get_train_test_data, make_predictions, test_preditions
+z
+from ..starter.starter.train_model import get_train_test_data
 
 @pytest.fixture(scope="module")
 def data():
@@ -9,35 +10,11 @@ def data():
 
     return data
 
-def get_test_data():
-    return get_train_test_data("starter/data/clean_census.csv", .2)
-
-def get_predictions():
-    preds, y_test = make_predictions(get_model(), "salary", get_encoder(), get_lb(), get_test_data())
-    preds, y_test = test_preditions(preds, y_test)
-    precision, recall, beta = test_preditions(preds, y_test)
-
-    return precision, recall, beta
-
 @pytest.fixture(scope="module")
-def precision():
-    precision, recall, beta = get_predictions()
-    return precision
+def data():
+    data = pd.read_csv("starter/data/clean_census.csv")
 
-@pytest.fixture(scope="module")
-def recall():
-    precision, recall, beta = get_predictions()
-    return recall
-
-@pytest.fixture(scope="module")
-def beta():
-    precision, recall, beta = get_predictions()
-    return beta
-
-def get_model():
-    lgbm_class = joblib.load('starter/model/lgbm_class.pkl')
-
-    return lgbm_class
+    return data
 
 @pytest.fixture(scope="module")
 def model():
@@ -51,21 +28,11 @@ def encoder():
 
     return encoder
 
-def get_encoder():
-    encoder = joblib.load('starter/model/encoder.pkl')
-
-    return encoder
-
 @pytest.fixture(scope="module")
 def lb():
     lb = joblib.load('starter/model/lb.pkl')
 
     return lb
-
-def get_lb():
-    lb = joblib.load('starter/model/lb.pkl')
-
-    return get_lb
 
 @pytest.fixture(scope="module")
 def true_positive():
--- tests/test_model.py
+++ tests/test_model.py
@@ -1,6 +1,6 @@
 import pandas as pd
-from ..starter.starter.ml.model import inference
 from ..starter.starter.ml.data import process_data
+from ..starter.starter.ml.model import inference
 
 def test_data_amount(data):
     assert 32500 < data.shape[0] < 1000000
@@ -22,16 +22,3 @@ def test_true_negative(true_negative, model,encoder,lb,cat_features):
     )
     preds = inference(model, X_test)
     assert(preds[0] == 0)
-
-def test_precision(precision):
-    """ The ratio between the True Positives and all the Positives."""
-    assert(precision >= .75)
-
-def test_recall(recall):
-    """ The measure of our model correctly identifying True Positives."""
-    assert(recall >= .60)
-
-def test_beta(beta):
-    """The Fbeta-measure measure is an abstraction of the F-measure where the balance of precision and recall
-     in the calculation of the harmonic mean is controlled by a coefficient called beta."""
-    assert(beta >= .70)
