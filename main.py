b780b880b3d978148b7e5507e966180e1476fe7d
Ben Kelly <Benjamin.J.Kelly@live.com>
2022-01-18 21:14:37 -0500

    Minor update

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from fastapi import FastAPI
 import joblib
 import pandas as pd
 import os
-from .starter.starter.ml.model import inference
-from .starter.starter.ml.data import process_data, get_cat_features
+from starter.starter.ml.model import inference
+from starter.starter.ml.data import process_data, get_cat_features
 
 from pydantic import BaseModel
 
