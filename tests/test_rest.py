3a73df8048f46fde04af86c2c1a7ebeb0bbe5fc6
Ben Kelly <Benjamin.J.Kelly@live.com>
2022-01-19 20:22:02 -0500

    Add REST test

---
+++ tests/test_rest.py
@@ -0,0 +1,17 @@
+from fastapi.testclient import TestClient
+sys.path.insert(0, os.getcwd())
+from main import app
+import pytest
+
+# Since I introduced model load on startup in main.py, there was an issue with testing
+# models as they were not loading. This structure allows to pass tests with async model load
+@pytest.fixture
+def client():
+    with TestClient(app) as client:
+        yield
+
+def test_get_root(client):
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {
+        "greeting": "Hello there!"}
